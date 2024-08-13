e425d930e79f4c86bb743089aa63f8c85d10153d
ocean <sa.ocean97@gmail.com>
2024-08-13 17:25:57 +0330

    JSON-diff implemented

--- javascript/json-diff.js
+++ javascript/json-diff.js
@@ -10,8 +10,42 @@
  */
 function jsonDiff(oldObject, newObject) {
   // TODO: Implement here
+  const res = {}
+  Object.keys(newObject).forEach(item => {
+    if (oldObject[item] && newObject[item] !== oldObject[item]) {
+      res[item] = {
+        type: "modified",
+        oldValue: oldObject[item],
+        newValue: newObject[item]
+      }
+      delete oldObject[item]
+    }
+    else if( oldObject[item] && newObject[item] === oldObject[item]) delete oldObject[item]
+    else if (!oldObject[item]) {
+      res[item] = {
+        type: "added",
+        newValue: newObject[item]
+      }
+    }
+  })
+  Object.keys(oldObject).map(item => res[item] = {
+    type: "removed",
+    oldValue: oldObject[item]
+  })
+  return res
 }
-
+const old = {
+  prop1: 1,
+  prop2: 2,
+  prop3: 3,
+  prop4: 4
+}
+const newObj = {
+  prop1: 1,
+  prop2: 22,
+  prop5: 5
+}
+console.log(jsonDiff(old, newObj))
 // {
 //     "key1": {
 //         "type": "modified",
